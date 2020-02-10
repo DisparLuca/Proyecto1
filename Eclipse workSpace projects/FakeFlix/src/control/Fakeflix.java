1546f3a65ed2d05e4f1d94016d9bf3e7bbb9680c
Davidubidu <davidfercios@gmail.com>
2020-02-10 17:07:37 +0100

    estoy modificando de prueba fakeflix

--- Eclipse workSpace projects/FakeFlix/src/control/Fakeflix.java
+++ Eclipse workSpace projects/FakeFlix/src/control/Fakeflix.java
@@ -1,5 +1,5 @@
 package control;
 
 public interface Fakeflix {
-
+	//hola, prueba.
 }
--- Eclipse_workSpace_projects/FakeFlix/src/control/Main.java
+++ Eclipse_workSpace_projects/FakeFlix/src/control/Main.java
@@ -1,9 +1,16 @@
 package control;
 
+import DAO.DAO;
+import DAO.DAOException;
+import DAO.FactoryDAO;
+import model.Film;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws DAOException {
+		
+		DAO<Film, Integer> dao = FactoryDAO.getInstance().getDAO();
+		dao.get(1);
 
 	}
 
